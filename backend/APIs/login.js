c2756a5227e71a18cb6ccbdc7a19ef0c801e5cf5
Nithin Ambati <nithinambati@Nithins-MacBook-Air.local>
2024-06-24 22:08:21 +0530

    DONe

--- backend/APIs/login.js
+++ backend/APIs/login.js
@@ -6,7 +6,6 @@ const jwt = require("jsonwebtoken");
 // Candidate Login API
 router.post("/user", async (req, res) => {
   const { email, password } = req.body;
-  console.log(req);
 
   try {
     // Find user by email and password
